f80e3f2a56a4465e0c27e2744d30d22f9bc2a62c
musharraf <musharrafaleem2@gmail.com>
2025-05-25 20:11:57 +0530

    created UserProfile model in users application

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    twitter_access_token = models.CharField(max_length=255, blank=True, null=True)
+    facebook_access_token = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
