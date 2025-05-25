8273a380776e5ebc976fddae560ae8f17bb3e2b8
musharraf <musharrafaleem2@gmail.com>
2025-05-25 21:43:21 +0530

    added contents under in views.py under integrations app

--- integrations/views.py
+++ integrations/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .twitter_client import fetch_recent_tweets
+
+def twitter_feed_view(request):
+    tweets = fetch_recent_tweets(username='musharrafaleem')  # Replace with your own username
+    return render(request, 'integrations/twitter_feed.html', {'tweets': tweets})
