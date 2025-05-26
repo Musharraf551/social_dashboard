7ae5f12cbf611079f092e9bd3f720d1d062f08c7
musharraf <musharrafaleem2@gmail.com>
2025-05-26 12:52:26 +0530

    created CustomUser model in users models.py

--- users/models.py
+++ users/models.py
@@ -11,3 +11,12 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
