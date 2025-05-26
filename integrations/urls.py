4b1f7871f2780080842bd6b31b8f869a949de56a
musharraf <musharrafaleem2@gmail.com>
2025-05-26 11:31:54 +0530

    done url mapping for facebook_feed_view

--- integrations/urls.py
+++ integrations/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('twitter/', views.twitter_feed_view, name='twitter-feed'),
+    path('facebook/', views.facebook_feed_view, name='facebook_feed'),
+
 ]
